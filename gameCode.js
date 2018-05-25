e39a5ea6e5ead003f711f54ea09cec0348d955cb
alex2293 <93maloy93@gmail.com>
2018-05-25 19:48:01 +0600

    дз 4 урок

---
+++ gameCode.js
@@ -0,0 +1,154 @@
+var event, ok;
+var mas = {};//Создаем пусой объект в который будем сохранять вопросы которые выводились в зависимости от выбора
+mas.v1 = works.a00;//Присваиваем первый вопрос
+
+
+do {//Выводим первый вопрос
+    ok = false;
+    event = +prompt(works.a00 + works.a1 + works.a2 + '-1 - Выход из игры');
+    if (event == -1) {
+        break;
+    }
+    else {
+        ok = isAnswer(works.a0, event);
+    }
+} while (!ok);
+switch (event) {
+    case 1: // Первое действие  - если в первом окне ввели 1 то открываем серию окон - окно 2
+        do {
+            ok = false;
+            event = +prompt(works.b00 + works.b1 + works.b2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                mas.v2 = works.b00;//Присваиваем 2 вопрос
+                ok = isAnswer(works.b0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие, если во 2 окне ввели 1 то переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        mas.v3 = works.d00;//Присваиваем 3 вопрос
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие   Если ввели 2 то также переходим на 4 окно
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        mas.v3 = works.d00;
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case 2: // Первое действие    Если в 1 окне ввели 2 то переходим к 3 окну
+        do {
+            ok = false;
+            event = +prompt(works.c00 + works.c1 + works.c2 + '-1 - Выход из игры');
+            if (event == -1) {
+                break;
+            }
+            else {
+                mas.v2 = works.c00;
+                ok = isAnswer(works.c0, event);
+            }
+        } while (!ok);
+        switch (event) {
+            case 1: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        mas.v3 = works.d00;
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case 2: // Второе действие
+                do {
+                    ok = false;
+                    event = +prompt(works.d00 + works.d1 + works.d2 + '-1 - Выход из игры');
+                    if (event == -1) {
+                        break;
+                    }
+                    else {
+                        mas.v3 = works.d00;
+                        ok = isAnswer(works.d0, event);
+                    }
+                } while (!ok);
+
+                break;
+            case -1: // Второе действие
+                break;
+            default:
+                alert('Ошибка');
+        }
+        break;
+    case -1: // Первое действие
+        break;
+    default:
+        alert('Ошибка');
+}
+
+//console.log(mas);
+do {//Выводим вопрос какой ход вывести,если -1 выйти,если нет выводим свойства объекта mas
+    ok=false;
+    var i = +prompt('Какой вопрос вы хотите увидеть?' + '\n-1 - Выход из игры');
+    if(i==-1){
+        break;
+    }
+    else{
+        switch  (i){
+                case 1: alert(mas.v1);
+                    break;
+                case 2: alert(mas.v2);
+                    break;
+                case 3:  alert(mas.v3);
+                    break;
+                case -1:
+                    break;
+                default : alert("Введены не коректные данные");
+                }
+       break;
+    }
+    
+}while (!ok)
+alert('Спасибо за игру');           
+            
+//------------------------------------------
+function isAnswer(q, event) {
+    if (isNaN(event) || !isFinite(event)) {
+        alert('Вы ввели недопустимый символ');
+        return false;
+    }
+    else if (event < 1 || event > q) {
+        alert('Ваше число выходит из допустимого диапозона');
+        return false;
+    }
+    return true;
+    
+}
